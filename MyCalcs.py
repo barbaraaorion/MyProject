1f34f6095a800f91bc21c66024720ac0ecc28ab5
BarbaraaOrion <barbara.kolem1@gmail.com>
2024-07-12 11:52:11 -0300

    Adicionando os testes de calculos

---
+++ MyCalcs.py
@@ -0,0 +1,18 @@
+# Demonstração do uso de operadores!
+X = int(input("Digite um número: "))
+Y = int(input("Digite outro número: "))
+
+SOMA = X + Y
+print("O resultado da adição é:", SOMA)
+
+SUBTRACAO = X - Y
+print("O resultado da subtração é:", SUBTRACAO)
+
+MULTIPLICACAO = X * Y
+print("O resultado da multiplicação é:", MULTIPLICACAO)
+
+DIVISAO = X / Y
+print("O resultado da divisão é:",DIVISAO)
+
+X = 10; Y = 20; Z = 30
+
